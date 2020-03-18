d4bf6ade1b26f4ffda7f458071282123f4adebd5
Renato Coutinho <renato.coutinho@gmail.com>
2020-03-17 22:45:48 -0300

    First attempt at an auto-updater. NOT PRODUCTION-READY YET.

---
+++ _src/auto-update.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+ROOT="$( dirname "${BASH_SOURCE[0]}" )"
+year=`date "+%Y"`
+hoje=`date +"%Y-%m-%d"`
+ontem=`date -d "yesterday" +"%Y-%m-%d"`
+
+python scraper/downloader.py
+
+if [ $? = 0 ]; then
+    R --no-save < update.R
+    if [ $? = 0 ]; then
+        pushd ..
+        git add outputs/prev.5d.csv  outputs/tempos.duplicacao.csv dados/brutos-ivis/ index.html
+        popd
+        git commit -m "[auto] Novos dados e projeções."
+        git push
+    else
+        echo "** Erro na atualização do site. **"
+    fi
+else
+    echo "** Nenhum dado novo, nenhuma projeção realizada. **"
+fi
+
