d497074d73b9cad51f6450d3ca914a286b21c48e
Vítor Sudbrack <49403656+vsudbrack@users.noreply.github.com>
2020-03-28 22:56:38 -0300

    Add files via upload

--- _src/update.R
+++ _src/update.R
@@ -3,6 +3,8 @@ source('ajuste_projecao_exponencial.R')
 source('plots.R')
 library(rmarkdown)
 
+source('../sp/_src/update.R')
+
 static_pages <- c('sobre.md', 'fontes.md', 'midia.md')
 dynamic_pages <- c('index.Rmd', 'informacoes.Rmd', 'projecao.Rmd', 'pais.Rmd', 'casos.Rmd', 'propagacao.Rmd', 'dinamica.Rmd', 'transmissao.Rmd')
 all_pages <- c(static_pages, dynamic_pages)
