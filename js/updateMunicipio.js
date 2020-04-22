b45b5bb6e297f927642205273b3b0a585dc69f81
badain <rafabadain@gmail.com>
2020-04-22 18:05:28 -0300

    updateMunicipio.js now changes between municipios

--- js/updateMunicipio.js
+++ js/updateMunicipio.js
@@ -6,7 +6,8 @@ const municipios =
             id: "sp_sp",
             verbose: "São Paulo"
         }
-    ];
+    ]
+;
 
 function getIndex(id) {
     for (i = 0; i < municipios.length; i++) {
@@ -23,7 +24,7 @@ function getVerbose(id) {
     }
 
     // UF not found: returns to default
-    console.log("getVerbose(): id not found");
+    console.log("getVerbose(): id not found "+id);
     return (def_mun_id);
 }
 
@@ -33,18 +34,11 @@ function getId(verbose) {
     }
 
     // UF not found: returns to default
-    console.log("getId(): verbose not found");
+    console.log("getId(): verbose not found "+verbose);
     return ("São Paulo");
 }
 
 /* Functions */
-function jquery_replace(attr, abstract, concrete) {
-    $(attr).each(function(index) {
-        original_text = $(this).html();
-        updated_text = original_text.replace(abstract, concrete);
-        $(this).html(updated_text);
-    });
-}
 
 function updatePage(current_id) {
     /* comportamento: atualiza conteudo dinamico de acordo com o municipio id */
@@ -66,38 +60,35 @@ function updatePage(current_id) {
 
     // conteudo dinamico
     // nome do municipio
-    classes_to_update = ['.card-title', '.card-text', '.text-justify'];
-    for(i = 0; i < classes_to_update.length; i++) jquery_replace(classes_to_update[i],"$municipio", current_state);
+    $(".municipio").text(current_state);
 
+    const regex = /"/gi;
     // forecast_exp (hospital)
     $.get('https://raw.githubusercontent.com/covid19br/covid19br.github.io/webdesign/web/data_forecasr_exp_municipios.csv', function (raw_data) {
-        var regex = /"/gi;
         full_data = raw_data.split("\n");
         current_data = full_data[current_index].replace(regex, '').split(" ");
-        jquery_replace('.forecast_exp',"$forecast_min", current_data[1]);
-        jquery_replace('.forecast_exp',"$forecast_max", current_data[2]);
-        jquery_replace('.forecast_exp',"$forecast_data", current_data[3]);
+        $(".forecast_min").text(current_data[1]);
+        $(".forecast_max").text(current_data[2]);
+        $(".forecast_data").text(current_data[3]);
     });
 
     // tempo_dupli (velocidade)
-    $.get('https://raw.githubusercontent.com/covid19br/covid19br.github.io/webdesign/web/data_tempo_dupli_municipios.csv', function (raw_data) {
-        var regex = /"/gi;
+    $.get('https://raw.githubusercontent.com/covid19br/covid19br.github.io/webdesign/web/data_tempo_dupli_municipio.csv', function (raw_data) {
         full_data = raw_data.split("\n");
         current_data = full_data[current_index].replace(regex, '').split(" ");
-        jquery_replace('.tempo_dupli',"$tempo_dupli_min", current_data[1]);
-        jquery_replace('.tempo_dupli',"$tempo_dupli_max", current_data[2]);
+        $(".tempo_dupli_min").text(current_data[1]);
+        $(".tempo_dupli_max").text(current_data[2]);
     });
 
     // RE (taxa)
-    $.get('https://raw.githubusercontent.com/covid19br/covid19br.github.io/webdesign/web/data_Re_municipios.csv', function (raw_data) {
-        var regex = /"/gi;
+    $.get('https://raw.githubusercontent.com/covid19br/covid19br.github.io/webdesign/web/data_Re_municipio.csv', function (raw_data) {
         full_data = raw_data.split("\n");
         current_data = full_data[current_index].replace(regex, '').split(" ");
-        jquery_replace('.re',"$re_min", current_data[1]);
-        jquery_replace('.re',"$re_max", current_data[2]);
-        if(current_data[1] >= 1) jquery_replace('.re',"$re_analise", "Mas mesmo o limite mínimo do intervalo de confiança está acima de um, indicando que a epidemia continua em expansão rápida.");
-        else if(current_data[2] < 1) jquery_replace('.re',"$re_analise", "No entanto, mesmo o limite máximo do intervalo de confiança está abaixo de um, indicando que a epidemia está em declínio");
-        else jquery_replace('.re',"$re_analise", "O limiar de 1 está dentro do intervalo de confiança, ou seja, $R_e$ pode ser maior ou menor que 1, então a epidemia pode estar em lento declínio ou expansão");
+        $(".re_min").text(current_data[1]);
+        $(".re_max").text(current_data[2]);
+        if(current_data[1] >= 1) $(".re_analise").text("Mas mesmo o limite mínimo do intervalo de confiança está acima de um, indicando que a epidemia continua em expansão rápida.");
+        else if(current_data[2] < 1) $(".re_analise").text("No entanto, mesmo o limite máximo do intervalo de confiança está abaixo de um, indicando que a epidemia está em declínio");
+        else $(".re_analise").text("O limiar de 1 está dentro do intervalo de confiança, ou seja, $R_e$ pode ser maior ou menor que 1, então a epidemia pode estar em lento declínio ou expansão");
     });
     
 }
@@ -115,8 +106,8 @@ else updatePage(def_mun_id); //se a entrada é sem hash atualiza para o municipi
 $(".dropdown-item").click(function () {
     // se nao eh o item atual
     if (!$(this).hasClass("active")) {
-        var current_mun = $(this).text();
-        updatePage(getId(current_mun));
+        var current_state = $(this).text();
+        updatePage(getId(current_state));
     }
 })
 
