daeaded5b4c1d2b14d11f301c50ecfff4832fae4
Renato Coutinho <renato.coutinho@gmail.com>
2020-05-08 16:35:57 -0300

    Robo comita arquivos de Re e TD municipio

--- _src/autoupdate.sh
+++ _src/autoupdate.sh
@@ -12,7 +12,7 @@ nova_pagina=$?
 if [[ $novos_dados = 0 && ! $nova_pagina = 0 ]]; then
     Rscript update.R && echo "Pagina BR atualizada" &&
     Rscript update_estado.R && echo "Pagina Estados atualizada" &&
-    git add ../outputs/*.prev.5d.csv ../outputs/*.tempos.duplicacao.csv ../outputs/tempos.duplicacao.csv ../outputs/prev.5d.csv ../web/est.tempo.dupl.html ../web/est.tempo.dupl.svg ../web/last.update.br.txt ../web/last.update.estado.txt ../web/plot.forecast.exp.*.html ../web/plot.forecast.exp.*.svg ../web/proj.num.casos.html ../web/proj.num.casos.svg ../web/data_forecast_exp_br.csv ../web/data_forecast_exp_estados.csv
+    git add ../outputs/*.prev.5d.csv ../outputs/*.tempos.duplicacao.csv ../outputs/tempos.duplicacao.csv ../outputs/prev.5d.csv ../web/est.tempo.dupl.html ../web/est.tempo.dupl.svg ../web/last.update.br.txt ../web/last.update.estado.txt ../web/plot.forecast.exp.*.html ../web/plot.forecast.exp.*.svg ../web/proj.num.casos.html ../web/proj.num.casos.svg ../web/data_forecast_exp_br.csv ../web/data_forecast_exp_estados.csv ../web/data_Re_municipio_srag.csv ../web/data_forecasr_exp_municipios_srag.csv ../web/data_tempo_dupli_municipio_srag.csv
 
     git commit -m "[auto] atualizando paginas BR e Estados" &&
     git push
