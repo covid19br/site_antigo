54c4d172d6b55a6f98164dd9d87ed7fa07111aec
badain <rafabadain@gmail.com>
2020-05-22 14:15:17 -0300

    lib_acumdia.js bugfix
    simplifica o código e remove bugs ao alternar entre gráficos

--- js/lib_acumdia.js
+++ js/lib_acumdia.js
@@ -1,63 +1,30 @@
-function updateWidgetAcumdia(request_id) {
-    // Get Widget's SRCs
-    var widget_src = "";
+function updateWidgetAcumdia(request_id, current_id) {
+    var new_src = ""; // prepare strings
 
-    // verifica se o primeiro grafico ja foi ativado. Se sim, pega a versao interativa
-    if ($(".acumdia.cov.nowcast > .codegena_iframe > iframe").length) widget_src = $(".acumdia.cov.nowcast > .codegena_iframe > iframe").attr("src");
-    else widget_src = $(".codegena_iframe.acumdia.cov.nowcast").attr("data-src");
-
-    if (widget_src.indexOf("nowcast") >= 0) {
-        // determina o id do path atual no site
-        var current_id = request_id;
-        if (widget_src.indexOf("_cum_") >= 0) current_id = "acu";
-        else current_id = "dia";
-        
-        var new_src = "";
-        if (request_id == current_id) return;
-        else if (request_id == "dia") {
-            new_src = widget_src.replace("_cum_", "_");
-        }
-        else if (request_id == "acu") {
-            new_src = widget_src.replace("nowcast_", "nowcast_cum_");
-        }
-        
-        // Update SRCs
-        $(".acumdia.cov.nowcast > .codegena_iframe > iframe").attr("src", new_src);
-        $(".acumdia.srag.nowcast > .codegena_iframe > iframe").attr("src", new_src.replace("covid", "srag"));
-        $(".acumdia.cov.ob > .codegena_iframe > iframe").attr("src", new_src.replace("_covid", "_ob_covid"));
-        $(".acumdia.srag.ob > .codegena_iframe > iframe").attr("src", new_src.replace("_covid", "_ob_srag_proaim"));
-    }
-    else return
-}
-
-function updatePlaceholderAcumdia(request_id) {
-    // Get Graph's SRCs
-    var graph_src = "";
-    
-    // verifica se o primeiro grafico ja foi ativado. Se sim, pega a versao interativa
-    if ($(".codegena_iframe.acumdia.cov.nowcast").length) {
-        graph_src = $(".codegena_iframe.acumdia.cov.nowcast").attr("data-src");
-    }
-    else {
-        graph_src = $(".acumdia.cov.nowcast > .codegena_iframe > iframe").attr("src");
-    }
-    
-    // change id
-    
-    // determina o id do path atual no site
-    var current_id = request_id;
-    if (graph_src.indexOf("_cum_") >= 0) current_id = "acu";
-    else current_id = "dia";
-    
-    // prepare strings
-    var new_src = "";
-
-    if (request_id == current_id) return; // se o requisitado e o atual sao iguais nao precisa atualizar
+    if (request_id == current_id) return;
     else if (request_id == "dia") {
-        new_src = graph_src.replace("_cum_", "_");
+        new_src = "./web/municipio_SP/plot_nowcast_covid.html";
     }
     else if (request_id == "acu") {
-        new_src = graph_src.replace("nowcast_", "nowcast_cum_");
+        new_src = "./web/municipio_SP/plot_nowcast_cum_covid.html";
+    }
+
+    // Update SRCs
+    $(".acumdia.cov.nowcast > .codegena_iframe > iframe").attr("src", new_src);
+    $(".acumdia.srag.nowcast > .codegena_iframe > iframe").attr("src", new_src.replace("covid", "srag"));
+    $(".acumdia.cov.ob > .codegena_iframe > iframe").attr("src", new_src.replace("_covid", "_ob_covid"));
+    $(".acumdia.srag.ob > .codegena_iframe > iframe").attr("src", new_src.replace("_covid", "_ob_srag_proaim"));
+}
+
+function updatePlaceholderAcumdia(request_id, current_id) {
+    var new_src = ""; // prepare strings
+
+    if (request_id == current_id) return;
+    else if (request_id == "dia") {
+        new_src = "./web/municipio_SP/plot_nowcast_covid.html";
+    }
+    else if (request_id == "acu") {
+        new_src = "./web/municipio_SP/plot_nowcast_cum_covid.html";
     }
 
     var new_svg = new_src.replace("html", "svg");
@@ -82,6 +49,7 @@ function updatePlaceholderAcumdia(request_id) {
 
 function updateAcumDia(request_id) {
     var selector = "." + request_id;
+    var current_id = $(".acumdia > .dropdown-item.active").attr("q");
     
     // DROPDOWN
     // list
@@ -103,6 +71,6 @@ function updateAcumDia(request_id) {
     $(".acum_ou_dia").text(request_verbose.toLowerCase());
 
     // GRAPHS
-    if ($(".placeholder_svg.acumdia").length) updatePlaceholderAcumdia(request_id) // placeholder image
-    if ($(".responsive_iframe").length) updateWidgetAcumdia(request_id);
+    if ($(".placeholder_svg.acumdia").length) updatePlaceholderAcumdia(request_id, current_id) // placeholder image
+    if ($(".responsive_iframe").length) updateWidgetAcumdia(request_id, current_id);
 }
