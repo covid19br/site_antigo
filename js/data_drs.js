7d5c0ba9ca1a46f9244e170e37f78a1441802f2a
Renato Coutinho <renato.coutinho@gmail.com>
2020-05-28 19:09:12 -0300

    Inicio html/js pra DRS

---
+++ js/data_drs.js
@@ -0,0 +1,11 @@
+const page_id = "DRS";
+const default_uf = "SP-Grande_Sao_Paulo";
+const default_verb = "Grande São Paulo";
+const locale =
+    [
+        {
+            uf: "SP-Grande_Sao_Paulo",
+            prep: "na",
+            verbose: "Grande São Paulo"
+        }
+    ];
