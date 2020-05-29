66d01cb9d60ad97c7e6f6c70f10aee755064ceba
Irina Lerner <iri.lerner@gmail.com>
2020-05-29 19:35:36 -0300

    Remove source("update_municipios.R")

--- _src/update_all.R
+++ _src/update_all.R
@@ -3,9 +3,3 @@ Sys.setlocale(category = "LC_TIME", locale = "pt_BR.UTF-8")
 source("update.R")
 source("update_estado.R")
 source("update_modelogro.R")
-
-municipios <- c("SP")
-
-for (mun in municipios){
-    source("update_municipio.R")
-}
