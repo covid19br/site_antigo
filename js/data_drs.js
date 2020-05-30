b05dafcc9a74b06b66ce049d2f67a420e39d2b5c
badain <rafabadain@gmail.com>
2020-05-30 17:35:21 -0300

    cinza todos os modelogros

--- js/data_drs.js
+++ js/data_drs.js
@@ -3,21 +3,21 @@ const default_uf = "SP-Grande_Sao_Paulo";
 const default_verb = "Grande São Paulo";
 const locale =
     [
-        { prep: "na", verbose: "Grande São Paulo",      uf: "SP-Grande_Sao_Paulo",      has_modelogro: true },
-        { prep: "em", verbose: "Araçatuba",             uf: "SP-Aracatuba",             has_modelogro: true },
-        { prep: "em", verbose: "Araraquara",            uf: "SP-Araraquara",            has_modelogro: true },
-        { prep: "na", verbose: "Baixada Santista",      uf: "SP-Baixada_Santista",      has_modelogro: true },
-        { prep: "em", verbose: "Barretos",              uf: "SP-Barretos",              has_modelogro: true },
-        { prep: "em", verbose: "Bauru",                 uf: "SP-Bauru",                 has_modelogro: true },
-        { prep: "em", verbose: "Campinas",              uf: "SP-Campinas",              has_modelogro: true },
-        { prep: "em", verbose: "Franca",                uf: "SP-Franca",                has_modelogro: true },
-        { prep: "em", verbose: "Marília",               uf: "SP-Marilia",               has_modelogro: true },
-        { prep: "em", verbose: "Piracicaba",            uf: "SP-Piracicaba",            has_modelogro: true },
-        { prep: "em", verbose: "Presidente Prudente",   uf: "SP-Presidente_Prudente",   has_modelogro: true },
-        { prep: "em", verbose: "Registro",              uf: "SP-Registro",              has_modelogro: true },
-        { prep: "em", verbose: "Ribeirão Preto",        uf: "SP-Ribeirao_Preto",        has_modelogro: true },
-        { prep: "em", verbose: "São João da Boa Vista", uf: "SP-Sao_Joao_da_Boa_Vista", has_modelogro: true },
-        { prep: "em", verbose: "São José do Rio Preto", uf: "SP-Sao_Jose_do_Rio_Preto", has_modelogro: true },
-        { prep: "em", verbose: "Sorocaba",              uf: "SP-Sorocaba",              has_modelogro: true },
-        { prep: "em", verbose: "Taubaté",               uf: "SP-Taubate",               has_modelogro: true }
+        { prep: "na", verbose: "Grande São Paulo",      uf: "SP-Grande_Sao_Paulo",      has_modelogro: false },
+        { prep: "em", verbose: "Araçatuba",             uf: "SP-Aracatuba",             has_modelogro: false },
+        { prep: "em", verbose: "Araraquara",            uf: "SP-Araraquara",            has_modelogro: false },
+        { prep: "na", verbose: "Baixada Santista",      uf: "SP-Baixada_Santista",      has_modelogro: false },
+        { prep: "em", verbose: "Barretos",              uf: "SP-Barretos",              has_modelogro: false },
+        { prep: "em", verbose: "Bauru",                 uf: "SP-Bauru",                 has_modelogro: false },
+        { prep: "em", verbose: "Campinas",              uf: "SP-Campinas",              has_modelogro: false },
+        { prep: "em", verbose: "Franca",                uf: "SP-Franca",                has_modelogro: false },
+        { prep: "em", verbose: "Marília",               uf: "SP-Marilia",               has_modelogro: false },
+        { prep: "em", verbose: "Piracicaba",            uf: "SP-Piracicaba",            has_modelogro: false },
+        { prep: "em", verbose: "Presidente Prudente",   uf: "SP-Presidente_Prudente",   has_modelogro: false },
+        { prep: "em", verbose: "Registro",              uf: "SP-Registro",              has_modelogro: false },
+        { prep: "em", verbose: "Ribeirão Preto",        uf: "SP-Ribeirao_Preto",        has_modelogro: false },
+        { prep: "em", verbose: "São João da Boa Vista", uf: "SP-Sao_Joao_da_Boa_Vista", has_modelogro: false },
+        { prep: "em", verbose: "São José do Rio Preto", uf: "SP-Sao_Jose_do_Rio_Preto", has_modelogro: false },
+        { prep: "em", verbose: "Sorocaba",              uf: "SP-Sorocaba",              has_modelogro: false },
+        { prep: "em", verbose: "Taubaté",               uf: "SP-Taubate",               has_modelogro: false }
     ];
