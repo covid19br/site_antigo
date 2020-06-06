11ecef016ab3e11da2bb0d9243836f013484f9c4
Renato Coutinho <renato.coutinho@gmail.com>
2020-06-05 21:05:38 -0300

    corrigindo erro no data_municipios.js

--- js/data_municipios.js
+++ js/data_municipios.js
@@ -7,6 +7,6 @@ const locale =
         { prep: "em", uf: "SP-Jundiai",          verbose: "Jundiaí",          has_modelogro: false, has_td: false },
         { prep: "em", uf: "MG-Belo_Horizonte",   verbose: "Belo Horizonte",   has_modelogro: false, has_td: true  },
         { prep: "no", uf: "RJ-Rio_de_Janeiro",   verbose: "Rio de Janeiro",   has_modelogro: false, has_td: true  },
-        { prep: "em", uf: "SC-Florianopolis",    verbose: "Florianópolis",    has_modelogro: false, has_td: true  }
+        { prep: "em", uf: "SC-Florianopolis",    verbose: "Florianópolis",    has_modelogro: false, has_td: true  },
         { prep: "em", uf: "PB-Joao_Pessoa",      verbose: "João Pessoa",      has_modelogro: false, has_td: true  }
     ];
