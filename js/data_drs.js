2340779a2d8547ae2111afa20227c4fd925f3f91
badain <rafabadain@gmail.com>
2020-06-07 00:19:54 -0300

    corrige diversos bugs relacionados à integração dos dados no site
    + css refresh

--- js/data_drs.js
+++ js/data_drs.js
@@ -1,23 +1,24 @@
 const page_id = "DRS";
-const default_uf = "SP-Grande_Sao_Paulo";
+const default_uf = "SP";
+const default_mun = "Grande_Sao_Paulo";
 const default_verb = "Grande São Paulo";
 const locale =
     [
-        { prep: "na", verbose: "Grande São Paulo",      uf: "SP-Grande_Sao_Paulo",      has_modelogro: true, has_td: true },
-        { prep: "em", verbose: "Araçatuba",             uf: "SP-Aracatuba",             has_modelogro: true, has_td: false },
-        { prep: "em", verbose: "Araraquara",            uf: "SP-Araraquara",            has_modelogro: true, has_td: false },
-        { prep: "na", verbose: "Baixada Santista",      uf: "SP-Baixada_Santista",      has_modelogro: true, has_td: false },
-        { prep: "em", verbose: "Barretos",              uf: "SP-Barretos",              has_modelogro: true, has_td: false },
-        { prep: "em", verbose: "Bauru",                 uf: "SP-Bauru",                 has_modelogro: true, has_td: false },
-        { prep: "em", verbose: "Campinas",              uf: "SP-Campinas",              has_modelogro: true, has_td: false },
-        { prep: "em", verbose: "Franca",                uf: "SP-Franca",                has_modelogro: true, has_td: false },
-        { prep: "em", verbose: "Marília",               uf: "SP-Marilia",               has_modelogro: true, has_td: false },
-        { prep: "em", verbose: "Piracicaba",            uf: "SP-Piracicaba",            has_modelogro: true, has_td: false },
-        { prep: "em", verbose: "Presidente Prudente",   uf: "SP-Presidente_Prudente",   has_modelogro: true, has_td: false },
-        { prep: "em", verbose: "Registro",              uf: "SP-Registro",              has_modelogro: true, has_td: false },
-        { prep: "em", verbose: "Ribeirão Preto",        uf: "SP-Ribeirao_Preto",        has_modelogro: true, has_td: false },
-        { prep: "em", verbose: "São João da Boa Vista", uf: "SP-Sao_Joao_da_Boa_Vista", has_modelogro: true, has_td: false },
-        { prep: "em", verbose: "São José do Rio Preto", uf: "SP-Sao_Jose_do_Rio_Preto", has_modelogro: true, has_td: false },
-        { prep: "em", verbose: "Sorocaba",              uf: "SP-Sorocaba",              has_modelogro: true, has_td: false },
-        { prep: "em", verbose: "Taubaté",               uf: "SP-Taubate",               has_modelogro: true, has_td: false }
+        { prep: "na", verbose: "Grande São Paulo",      uf:"SP", mun:"Grande_Sao_Paulo",      has_modelogro: true, has_td: true },
+        { prep: "em", verbose: "Araçatuba",             uf:"SP", mun:"Aracatuba",             has_modelogro: true, has_td: false },
+        { prep: "em", verbose: "Araraquara",            uf:"SP", mun:"Araraquara",            has_modelogro: true, has_td: false },
+        { prep: "na", verbose: "Baixada Santista",      uf:"SP", mun:"Baixada_Santista",      has_modelogro: true, has_td: false },
+        { prep: "em", verbose: "Barretos",              uf:"SP", mun:"Barretos",              has_modelogro: true, has_td: false },
+        { prep: "em", verbose: "Bauru",                 uf:"SP", mun:"Bauru",                 has_modelogro: true, has_td: false },
+        { prep: "em", verbose: "Campinas",              uf:"SP", mun:"Campinas",              has_modelogro: true, has_td: false },
+        { prep: "em", verbose: "Franca",                uf:"SP", mun:"Franca",                has_modelogro: true, has_td: false },
+        { prep: "em", verbose: "Marília",               uf:"SP", mun:"Marilia",               has_modelogro: true, has_td: false },
+        { prep: "em", verbose: "Piracicaba",            uf:"SP", mun:"Piracicaba",            has_modelogro: true, has_td: false },
+        { prep: "em", verbose: "Presidente Prudente",   uf:"SP", mun:"Presidente_Prudente",   has_modelogro: true, has_td: false },
+        { prep: "em", verbose: "Registro",              uf:"SP", mun:"Registro",              has_modelogro: true, has_td: false },
+        { prep: "em", verbose: "Ribeirão Preto",        uf:"SP", mun:"Ribeirao_Preto",        has_modelogro: true, has_td: false },
+        { prep: "em", verbose: "São João da Boa Vista", uf:"SP", mun:"Sao_Joao_da_Boa_Vista", has_modelogro: true, has_td: false },
+        { prep: "em", verbose: "São José do Rio Preto", uf:"SP", mun:"Sao_Jose_do_Rio_Preto", has_modelogro: true, has_td: false },
+        { prep: "em", verbose: "Sorocaba",              uf:"SP", mun:"Sorocaba",              has_modelogro: true, has_td: false },
+        { prep: "em", verbose: "Taubaté",               uf:"SP", mun:"Taubate",               has_modelogro: true, has_td: false }
     ];
