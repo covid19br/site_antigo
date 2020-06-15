67d8ac451d869e1a9e61c812295d68ee3ae0262f
Renato Coutinho <renato.coutinho@gmail.com>
2020-06-15 14:03:31 -0300

    + SBO

--- js/data_municipios.js
+++ js/data_municipios.js
@@ -41,6 +41,7 @@ const locale =
         { prep: "em", uf: "AM", mun:"Manacapuru",            verbose: "Manacapuru",            has_modelogro: false, has_td: false  },
         { prep: "em", uf: "RJ", mun:"Niteroi",               verbose: "Niterói",               has_modelogro: false, has_td: false },
         { prep: "em", uf: "SP", mun:"Ribeirao_Preto",        verbose: "Ribeirão Preto",        has_modelogro: false, has_td: false  },
+        { prep: "em", uf: "SP", mun:"Santa_Barbara_dOeste",  verbose: "Santa Bárbara d'Oeste", has_modelogro: false, has_td: false  },
         { prep: "em", uf: "PR", mun:"Londrina",              verbose: "Londrina",              has_modelogro: false, has_td: true  },
         { prep: "em", uf: "PR", mun:"Maringa",               verbose: "Maringá",               has_modelogro: false, has_td: false  },
         { prep: "em", uf: "PE", mun:"Olinda",                verbose: "Olinda",                has_modelogro: false, has_td: false  },
