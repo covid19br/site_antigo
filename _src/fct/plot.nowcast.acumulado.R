49a0879f3149e3ccdffccde9e07b373bdc4ec4c3
Andrea Sánchez Tapia <katori@gmail.com>
2020-06-17 00:15:51 -0700

    resolve bug en gráficos

--- _src/fct/plot.nowcast.acumulado.R
+++ _src/fct/plot.nowcast.acumulado.R
@@ -1,21 +1,21 @@
 # Funcao para fazer o plot de nowcasting acumulado, com valores estimados e notificados, com projecao para os proximos 5 dias
 plot.nowcast.acumulado <- function(df){
     plot <- df %>%
-        mutate(data = as.Date(data)) %>% 
+        mutate(data = as.Date(data)) %>%
         ggplot(aes(x = data)) +
+        # ic
+        geom_ribbon(aes(ymin = now.low.c.proj, ymax = now.upp.c.proj), fill = "lightgrey") +
+        # ic
+        geom_ribbon(aes(ymin = not.low.c.proj, ymax = not.upp.c.proj), fill = "lightgrey") +
         # linha e ponto
         geom_line(aes(y = now.mean.c, color = "Estimados"), size = 1) +
         #geom_point(aes(y = now.mean.c, color = "Estimados"), size = 1) +
-        # ic
-        geom_ribbon(aes(ymin = now.low.c.proj, ymax = now.upp.c.proj), fill = "lightgrey") +
         # linha e ponto projecao
         geom_line(aes(y = now.mean.c.proj, color = "Estimados"), lty = "longdash") +
         #geom_point(aes(y = now.mean.c.proj, color = "Estimados")) +
-        # linha e ponto 
+        # linha e ponto
         geom_line(aes(y = not.mean.c, color = "Notificados"), size = 1) +
         #geom_point(aes(y = not.mean.c, color = "Notificados"), size = 1) +
-        # ic
-        geom_ribbon(aes(ymin = not.low.c.proj, ymax = not.upp.c.proj), fill = "lightgrey") +
         # linha e ponto projecao
         geom_line(aes(y = not.mean.c.proj, color = "Notificados"), lty = "longdash") +
         #geom_point(aes(y = not.mean.c.proj, color = "Notificados"), size = 1) +
--- _src/fct/plot.nowcast.diario.R
+++ _src/fct/plot.nowcast.diario.R
@@ -3,11 +3,11 @@ plot.nowcast.diario <- function(df) {
     plot <- df %>%
         mutate(data = as.Date(data)) %>%
         ggplot(aes(x = data)) +
+        geom_ribbon(aes(ymin = lower.merged.pred, ymax = upper.merged.pred),
+                    fill = RColorBrewer::brewer.pal(3, "Set1")[2], alpha = 0.1) +
         geom_line(aes(y = estimate.merged), lty = 2, col = "grey") +
         geom_point(aes(y = n.casos, col = "Notificado"), size = 2) +
         geom_point(aes(y = estimate, col = "Nowcasting"), size = 2) +
-        geom_ribbon(aes(ymin = lower.merged.pred, ymax = upper.merged.pred),
-                    fill = RColorBrewer::brewer.pal(3, "Set1")[2], alpha = 0.1) +
         geom_line(aes(y = estimate.merged.smooth), alpha = 0.6, size = 2) +
         scale_x_date(date_labels = "%d/%b") +
         scale_color_manual(name = "", values = RColorBrewer::brewer.pal(3, "Set1")[2:1]) +
@@ -15,5 +15,5 @@ plot.nowcast.diario <- function(df) {
         ylab("Número de novos casos") +
         plot.formatos +
         theme(legend.position = "none")
-    plot        
+    plot
 }
