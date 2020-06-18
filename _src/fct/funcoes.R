e5785b805b7a8d0f793c5e592ede8896c76a1a4b
rafalopespx <lopes1313@gmail.com>
2020-06-18 16:26:34 -0300

    atualização de funções.R com beta.cumsum, com if agora

--- _src/fct/funcoes.R
+++ _src/fct/funcoes.R
@@ -131,6 +131,7 @@ beta.summary <- function(NobBS.output, NobBS.params.post){
 #' @param NobBS.params.post data frame com as distribuicoes
 #'     posteriores dos parâmetros estimados pela função NobBS. Está
 #'     contido na lista que é retornada pela função.
+#' @param samples Número de samples a ser considerado.
 #' @return data frame com média e quantis 2.5% e 97.5% das
 #'     distribuições a posteriori dos parâmetros de atraso de
 #'     notificação pelo método de nowcasting da função NobBS. Os
@@ -144,14 +145,17 @@ beta.cumsum <- function(NobBS.output, NobBS.params.post, samples){
     else
         df <- nowcasting$params.post
         df1 <- df[, names(df)[grepl("Beta",names(df))]]
-        df2<-df1[sample(nrow(df1), samples), ]
-        df2<-exp(df2)
-        df3<-t(apply(df2, 1, cumsum))
-        data.frame(atraso = as.integer(substr(colnames(df3), 6, 8)),
-                    mean = apply(df3, 2, mean),
-                    lower = apply(df3, 2, quantile, 0.025),
-                    upper = apply(df3, 2, quantile, 0.975),
-                    row.names = names(df3))
+        if (samples > nrow(df1))
+            stop(paste("samples deve ter tamanho menor ou igual a", nrow(df1)))
+        else
+            df2<-df1[sample(nrow(df1), samples), ]
+            df2<-exp(df2)
+            df3<-t(apply(df2, 1, cumsum))
+            data.frame(atraso = as.integer(substr(colnames(df3), 6, 8)),
+                        mean = apply(df3, 2, mean),
+                        lower = apply(df3, 2, quantile, 0.025),
+                        upper = apply(df3, 2, quantile, 0.975),
+                        row.names = names(df3))
 }
 
 #' Estima numero de notificacoes por dia a partir de um vetor de n de
